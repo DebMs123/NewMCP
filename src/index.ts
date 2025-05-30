1ecb7d1708edaa1c22fd72889ffbee46524b10bd
DebMs123 <142893580+DebMs123@users.noreply.github.com>
2025-05-30 11:54:42 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,8 @@ const servernext = new McpServer({
   version: "1.0.0",
   tools: [
     {
-      name: "get-power",
-      description: "Get a random Power",
+      name: "get-power-automate",
+      description: "Get Power Automate",
       parameters: {}
     },
   });
@@ -120,10 +120,10 @@ const getYoMamaJoke = server.tool(
   }
 );
 
-// Get Power tool
-const getPower = servernext.tool(
-  "get-power",
-  "Get a random Power",
+// Get Power Automate tool
+const getPowerAutomate = servernext.tool(
+  "get-power-automate",
+  "Get Power Automate",
   async () => {
     const response = await fetch(
       "https://prod-175.westus.logic.azure.com:443/workflows/ef240e9705ca470c9102158cc9f00f09/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=QvEO0KDDeDSkmMd5Juog-f54qJ7TZubYij8TnNlJ-6s"
